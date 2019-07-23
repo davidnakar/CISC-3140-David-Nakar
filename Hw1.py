04e43df7779c095900022a0dee0075629e6a2e51
davidnakar <45132178+davidnakar@users.noreply.github.com>
2019-07-23 18:35:00 -0400

    Add files via upload

---
+++ Hw1.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import urllib.request, json
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def print_nasa_data():
+ with urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=Vg0VbaeecjP5l8HWudpPzpmdOIeG7FeQXbcknic1 ") as url:
+        data = json.loads(url.read())
+
+        return data
+
+if __name__ == '__main__':
+    app.run()
